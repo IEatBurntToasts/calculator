220a754b5280ecfa9d57826272d66e98a5a7bc95
Quan <61860115+Dracozure@users.noreply.github.com>
2024-06-14 18:17:27 -0700

    Add enter/equal functionality

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ window.onload = () => {
 let visualDisplay = document.querySelector('.main-display');
 let subVisualDisplay = document.querySelector('.sub-display');
 let currentOperation = '';
-let enterSecondTerm = false;
+let resetDisplay = false;
 
 function processInput(key) {
     const numberKeys = [0,1,2,3,4,5,6,7,8,9];
     const operators = ['+','-','/','*'];
-    const deleteKey = 'backspace';
-    const enterKey = 'enter';
 
     const keyType = () => {
         if (numberKeys.includes(+key)) {
@@ -33,6 +31,9 @@ function processInput(key) {
         case 'backspace':
             processBackspaceInput();
             break;
+        case 'enter':
+            processEnterInput();
+            break;
     }
 }
 
@@ -42,36 +43,36 @@ function processOperatorInput(operator) {
     if (operationTerms.length !== 2) {
         currentOperation = visualDisplay.textContent + ' ' + operator;
         subVisualDisplay.textContent = currentOperation;
-        enterSecondTerm = true;
+        resetDisplay = true;
         return;
-    } else if (enterSecondTerm === true) {
+    } else if (resetDisplay === true) {
         currentOperation = operationTerms[0] + ' ' + operator;
         subVisualDisplay.textContent = currentOperation;
         return;
     }
 
-    const currentOperator = currentOperation.split(' ');
+    const currentOperator = operationTerms[1];
     let result;
 
-    switch (currentOperator[1]) {
-        case '+':
-            result = addNumbers(operationTerms[0], visualDisplay.textContent);
-            break;
-        case '-':
-            result = subtractNumbers(operationTerms[0], visualDisplay.textContent);
-            break;
-        case '*':
-            result = multiplyNumbers(operationTerms[0], visualDisplay.textContent);
-            break;
-        case '/':
-            result = divideNumbers(operationTerms[0], visualDisplay.textContent);
-            break;
-    }
+    result = processOperationType(currentOperator, operationTerms[0], visualDisplay.textContent);
 
     visualDisplay.textContent = result;
     currentOperation = '';
 
     processOperatorInput(operator);
+}
+
+function processOperationType(operator, operand1, operand2) {
+    switch (operator) {
+        case '+':
+            return addNumbers(operand1, operand2);
+        case '-':
+            return subtractNumbers(operand1, operand2);
+        case '*':
+            return multiplyNumbers(operand1, operand2);
+        case '/':
+            return divideNumbers(operand1, operand2);
+    }
 }
 
 function divideNumbers(operand1, operand2) {
@@ -90,13 +91,28 @@ function subtractNumbers(operand1, operand2) {
     return operand1 - operand2;
 }
 
+function processEnterInput() {
+    const operationTerms = currentOperation.split(' ');
+    let result;
+
+    if (currentOperation === '') {
+        return;
+    }
+
+    result = processOperationType(operationTerms[1], operationTerms[0], visualDisplay.textContent);
+    subVisualDisplay.textContent = `${operationTerms[0]} ${operationTerms[1]} ${visualDisplay.textContent} =`;
+    visualDisplay.textContent = result;
+    currentOperation = '';
+    resetDisplay = true;
+}
+
 function processNumberInput(number) {
-    if (+visualDisplay.textContent === 0 || enterSecondTerm === true) {
+    if (+visualDisplay.textContent === 0 || resetDisplay === true) {
         visualDisplay.textContent = '';
     }
 
     visualDisplay.textContent = visualDisplay.textContent + number;
-    enterSecondTerm = false;
+    resetDisplay = false;
 }
 
 function processBackspaceInput() {
